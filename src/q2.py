884edf7f6d7be609b7ef1571760f6d3334951f1c
mashu3i33245 <168373396+mashu3i33245@users.noreply.github.com>
2026-01-20 11:13:19 -0500

    Update q2.py

--- src/q2.py
+++ src/q2.py
@@ -15,4 +15,7 @@ def set_password() -> None:
        (Keep repeating until password is valid)
     """
     # This function will require calling validate_password from q1
-    pass
+    while True:
+        password = input("Enter a password: ")
+        if validate_password(password):
+            break
